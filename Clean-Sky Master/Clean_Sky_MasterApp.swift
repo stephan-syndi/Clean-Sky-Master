7106c6b04b30a4c18d8f4f5c7d7c4e90bb67f20c
Stephan <stepandegtsiaryk@syndi-g.com>
2026-02-27 11:25:38 +0300

    Initial Commit

---
+++ Clean-Sky Master/Clean_Sky_MasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clean_Sky_MasterApp.swift
+//  Clean-Sky Master
+//
+//  Created by Stepan Degtsiaryk on 27.02.26.
+//
+
+import SwiftUI
+
+@main
+struct Clean_Sky_MasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Clean-Sky Master/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Clean-Sky Master
+//
+//  Created by Stepan Degtsiaryk on 27.02.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
